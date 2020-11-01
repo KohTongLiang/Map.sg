b8abfa7add9d0af77e14c2d0c0cafe951e6e4a2a
KohTongLiang <koh.tongliang@gmail.com>
2020-11-02 03:44:45 +0800

    Fixed conflict

--- application/src/Components/Map/index.js
+++ application/src/Components/Map/index.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { connect } from "react-redux";
-<<<<<<< Updated upstream
 import { Gantry } from '../../json/Gantry';
 import { GantryCoordinates } from '../../json/GantryCoordinates';
-=======
-//import Gantry from '../../json/Gantry';
->>>>>>> Stashed changes
 import * as turf from '@turf/turf';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
